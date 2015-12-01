2d3f2b5d0ab1317775cef8b9980d1e260d0c0d17
nhanle2904 <nhanle2904@gmail.com>
2015-11-30 16:33:12 -0800

    test code

--- Final Project/andgate.vhd
+++ Final Project/andgate.vhd
@@ -2,12 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity andGate is
-   port( A : in std_logic;
+   port( A , C: in std_logic;
 	B : in std_logic;
-            F : out std_logic);
+            F ,k: out std_logic_vector(4 downto 0));
 end andGate;
 
 architecture func of andGate is
 begin
    F <= A and B;
+C;
+K;
 end func;
